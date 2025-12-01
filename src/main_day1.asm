dac426cd87c2c08af3e67593c4ad2f36c8282a92
Topi Talvitie <topi.talvitie@iki.fi>
2025-12-01 22:14:00 +0200

    Solve day 1 second star.

--- src/main_day1.asm
+++ src/main_day1.asm
@@ -24,7 +24,10 @@ main:
 
     %assign %$localsize 0
     %local dial_pos:dword
-    %local zero_count:dword
+    %local stop_at_zero_count:dword
+    %local pass_zero_count:dword
+    %local prev_dial_round:dword
+    %local prev_dial_remainder:dword
 
     push ebp
     mov ebp, esp
@@ -44,7 +47,10 @@ main:
     ; Read input (read pos ebx), tracking dial position.
     mov ebx, input
     mov dword [dial_pos], 50
-    mov dword [zero_count], 0
+    mov dword [stop_at_zero_count], 0
+    mov dword [pass_zero_count], 0
+    mov dword [prev_dial_round], 0
+    mov dword [prev_dial_remainder], 50
 .loop:
     ; Break loop when 0-byte is reached
     cmp byte [ebx], 0
@@ -81,17 +87,55 @@ main:
     mul esi
     add [dial_pos], eax
 
-    ; Compute dial_pos % 100 to edx
+    ; Compute dial_pos / 100 to eax and dial_pos % 100 to edx
     mov eax, [dial_pos]
     cdq
     mov ecx, 100
     idiv ecx
 
-    ; If the remainder is 0, increase counter
+    ; Fix eax to floor(dial_pos / 100) instead of towards-zero rounding
+    cmp dword [dial_pos], 0
+    jge .no_fix_needed
     cmp edx, 0
-    jne .remainder_nonzero
-    inc dword [zero_count]
-.remainder_nonzero:
+    je .no_fix_needed
+    dec eax
+.no_fix_needed:
+
+    ; Increase the counter for zero passes by the change in floor(dial_pos / 100)
+    push eax
+    sub eax, [prev_dial_round]
+    cmp eax, 0
+    jge .rounds_not_negative
+    neg eax
+.rounds_not_negative:
+    add [pass_zero_count], eax
+    pop eax
+
+    ; If the remainder is 0, increase counter for stop at zero
+    cmp edx, 0
+    jne .remainder_check_done
+    inc dword [stop_at_zero_count]
+
+    ; For right rotations, we also need to reduce the zero pass count to avoid double-counting the
+    ; last movement
+    cmp esi, 0
+    jle .remainder_check_done
+    dec dword [pass_zero_count]
+.remainder_check_done:
+
+    ; If we did a left-rotation from remainder-zero position, we also need to reduce the zero pass
+    ; count, to avoid double-counting the last movement
+    cmp dword [prev_dial_remainder], 0
+    jne .left_from_zero_check_done
+    cmp esi, 0
+    jge .left_from_zero_check_done
+    dec dword [pass_zero_count]
+.left_from_zero_check_done:
+
+
+    ; Save floor(dial_pos / 100) and the remainder for next round
+    mov [prev_dial_round], eax
+    mov [prev_dial_remainder], edx
 
     ; Consume newline
     cmp byte [ebx], `\n`
@@ -105,15 +149,27 @@ main:
 
 .done:
     ; Generate output string
-    push output_capacity - 1
-    push output
-    push dword [zero_count]
+    mov edi, output
+    push output_capacity / 2 - 1
+    push edi
+    push dword [stop_at_zero_count]
     call uint_to_str
-    mov byte [output+eax], `\n`
-    inc eax
+    add edi, eax
+    mov byte [edi], `\n`
+    inc edi
+    push output_capacity / 2 - 1
+    push edi
+    mov eax, [pass_zero_count]
+    add eax, [stop_at_zero_count]
+    push eax
+    call uint_to_str
+    add edi, eax
+    mov byte [edi], `\n`
+    inc edi
+    sub edi, output
 
     ; Write output to stdout
-    push eax
+    push edi
     push output
     call write_all_stdout
 
